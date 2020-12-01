da75fff0484822a59e767e8c5b786aca0a240e57
Crhistian Alexander Garcia Urbano <42595236+crhistian2296@users.noreply.github.com>
2020-12-01 18:02:05 -0500

    242. donee

--- 242. Valid Anagram.js
+++ 242. Valid Anagram.js
@@ -3,21 +3,32 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function (s, t) {
-    if (s.length == t.length) {
-        let perfectMatch = 0;
-        for (let sIndex = 0; sIndex < (s.length); sIndex++) {
-            for (let tIndex = 0; tIndex < (t.length); tIndex++) {
-                if (s[sIndex] == t[tIndex]) {
-                    perfectMatch += 1;
-                    t = t.split("t[tIndex]").join("");
-                    continue;
-                }
-            }
-        }
-        if (t == "") {
-            return true;
+function isAnagram(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    let letterCount = {};
+
+    for (let letter of s) {
+        if (letterCount[letter]) {
+            letterCount[letter]++;
+        } else {
+            letterCount[letter] = 1;
         }
     }
-    return false;
-}
+
+    for (let letter of t) {
+        if (letterCount[letter] === undefined) {
+            return false;
+        }
+
+        if (letterCount[letter] < 1) {
+            return false;
+        }
+
+        letterCount[letter]--;
+    }
+
+    return true;
+};
