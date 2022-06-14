f9fa2464bb105e47085509c6940369f41fff76d5
crhistian garcia <garcia.crhistian@correounvalle.edu.co>
2022-06-14 16:35:05 +0200

    35. search insert position

---
+++ 35. search insert position.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  let max = nums.length - 1;
+  let min = 0;
+  let mid = 0;
+
+  while (max - min >= 1) {
+    mid = Math.floor((max + min) / 2);
+    if (nums[mid] < target) min = mid + 1;
+    else max = mid;
+  }
+
+  if (nums[min] === target) return min;
+  else if (nums[max] === target) return max;
+  else if (nums[min] > target) return min;
+  // else if (nums[max] > target) return min;
+  else return min + 1;
+};
+
+console.log(searchInsert([1, 3, 5, 6], 2));
